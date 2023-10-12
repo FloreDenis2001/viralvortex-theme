f0cbfa0baad4cc83222d7a0da6e0dd8e7ac699b4
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-12 19:40:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let header = document.querySelector(".header");
+let nodes = document.querySelectorAll('.about__item');
+
+
+nodes.forEach((node, index) => {
+    if ((index + 1) % 2 === 0) {
+        node.style.marginTop = '200px';
+    }
+});
+window.addEventListener("scroll", () => {
+    
+    if (window.scrollY > 130) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+});
+
+});

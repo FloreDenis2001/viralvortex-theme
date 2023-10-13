6da5022aece6682025f23a8a00878182aa487e58
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-13 21:15:00 +0300

    last_update

--- script.js
+++ script.js
@@ -2,7 +2,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     let header = document.querySelector(".header");
 let nodes = document.querySelectorAll('.about__item');
+let btns=document.querySelectorAll('.nav');
 
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+        let activeButton = document.querySelector(".active");
+        
+        if (activeButton) {
+            activeButton.classList.remove("active");
+        }
+
+        this.classList.add("active");
+    });
+}
 
 nodes.forEach((node, index) => {
     if ((index + 1) % 2 === 0) {
@@ -18,8 +31,21 @@ window.addEventListener("scroll", () => {
     }
 });
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
 
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 });
